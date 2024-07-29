dc23b4b75274ef2fa69e5ab259e623c6070b237b
leeya018 <leeyahav018@gmail.com>
2024-07-29 12:37:56 +0300

    x

--- next.config.mjs
+++ next.config.mjs
@@ -14,32 +14,37 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "66a74c3e0aed3b0e0486f6e2--shosh-gallery.netlify.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/_next/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Allows all origins
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/_next/:path*",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*", // Allows all origins
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,POST,PUT,DELETE,OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "X-Requested-With, Content-Type, Authorization",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
